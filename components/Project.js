f93dbc3a46261d27beced3f04a15e9335dbdae33
kejiojo <ojokeji@gmail.com>
2021-06-13 18:14:36 +0100

    project

--- components/Project.js
+++ components/Project.js
@@ -1,7 +1,20 @@
+import {useState} from 'react';
 import styles from '../styles/Projects.module.scss';
+import { CgArrowLongRight } from "react-icons/cg";
 
 
 const Project = () => {
+
+  const [right, setRight] = useState(true);
+
+    const btnHover = () => {
+        setRight(false);
+    }
+
+    const btnLeave = () => {
+        setRight(true);
+    }
+
     return (
 
       <div>
@@ -13,23 +26,36 @@ const Project = () => {
                 <div className={styles.container}>
                     <div className={styles.title}>Novela</div>
                     <div className={styles.image}>
-                        <img src="./images/novela.png" alt="novela image" />
+                        <img className={styles.img} src="./images/novela.png" alt="novela image" />
                     </div>
+                </div>
+
+           <div className={styles.button} onMouseEnter={btnHover} onMouseLeave={btnLeave}>
+             <div className={styles.btn}>
+                  View More
+                <div className={ `${styles["icon"]} ${styles[right ? "" : "right-active"]}`}>
+                  <CgArrowLongRight size="1.9rem" />  
                 </div>
                 
-                <div className={ `${styles["container"]} ${styles["contain"]}`}>
-                    <div className={ `${styles["title"]} ${styles["titles"]}`}>Gateway</div>
-                    <div className={styles.image}>
-                        <img src="./images/gateway.png" alt="gateway image" />
-                    </div>
-                </div>
+             </div>
+          </div>
+                
+               
             </div>
 
             <div className={styles.right}>
                <div className={styles.container}>
                     <div className={styles.title}>Eko-Gallery</div>
                     <div className={styles.image}>
-                        <img src="./images/eko.png" alt="eko image" />
+                        <img  src="./images/eko.png" alt="eko image" />
+                    </div>
+                </div>
+
+
+                 <div className={ `${styles["container"]} ${styles["contain"]}`}>
+                    <div className={ `${styles["title"]} ${styles["titles"]}`}>Gateway</div>
+                    <div className={ `${styles["image"]} ${styles["images"]}`}>
+                        <img src="./images/gateway.png" alt="gateway image" />
                     </div>
                 </div>
             </div>
