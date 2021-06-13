d8b7aa31c8af58d70739f43656ab647ed93626bb
kejiojo <ojokeji@gmail.com>
2021-06-13 19:18:52 +0100

    projects

--- components/Project.js
+++ components/Project.js
@@ -6,6 +6,9 @@ import { CgArrowLongRight } from "react-icons/cg";
 const Project = () => {
 
   const [right, setRight] = useState(true);
+  const [novela, setNovela] = useState(true);
+  const [eko, setEko] = useState(true);
+  const [gate, setGate] = useState(true);   
 
     const btnHover = () => {
         setRight(false);
@@ -15,6 +18,32 @@ const Project = () => {
         setRight(true);
     }
 
+    const novelaHover = () => {
+        setNovela(false);
+    }
+
+     const novelaLeave = () => {
+        setNovela(true);
+    }
+
+      const ekoHover = () => {
+        setEko(false);
+    }
+
+     const ekoLeave = () => {
+        setEko(true);
+    }
+
+   const gateHover = () => {
+        setGate(false);
+    }
+
+     const gateLeave = () => {
+        setGate(true);
+    }
+
+   
+
     return (
 
       <div>
@@ -23,9 +52,9 @@ const Project = () => {
 
         <div className={styles.project}>
             <div className={styles.left}>
-                <div className={styles.container}>
-                    <div className={styles.title}>Novela</div>
-                    <div className={styles.image}>
+                <div className={styles.container} onMouseEnter={novelaHover} onMouseLeave={novelaLeave}>
+                    <div className={ `${styles["title"]} ${styles[novela ? "" : "title-active"]}`}>Novela</div>
+                    <div className={ `${styles["image"]} ${styles[novela ? "" : "image-active"]}`}>
                         <img className={styles.img} src="./images/novela.png" alt="novela image" />
                     </div>
                 </div>
@@ -44,17 +73,17 @@ const Project = () => {
             </div>
 
             <div className={styles.right}>
-               <div className={styles.container}>
-                    <div className={styles.title}>Eko-Gallery</div>
-                    <div className={styles.image}>
+               <div className={styles.container} onMouseEnter={ekoHover} onMouseLeave={ekoLeave}>
+                    <div className={ `${styles["title"]} ${styles[eko ? "" : "title-active"]}`}>Eko-Gallery</div>
+                    <div className={ `${styles["image"]} ${styles[eko ? "" : "image-active"]}`}>
                         <img  src="./images/eko.png" alt="eko image" />
                     </div>
                 </div>
 
 
-                 <div className={ `${styles["container"]} ${styles["contain"]}`}>
-                    <div className={ `${styles["title"]} ${styles["titles"]}`}>Gateway</div>
-                    <div className={ `${styles["image"]} ${styles["images"]}`}>
+                 <div className={ `${styles["container"]} ${styles["contain"]}`} onMouseEnter={gateHover} onMouseLeave={gateLeave}>
+                    <div className={ `${styles["title"]} ${styles["titles"]} ${styles[gate ? "" : "title-active"]}`}>Gateway</div>
+                    <div className={ `${styles["image"]} ${styles["images"]} ${styles[gate ? "" : "images-active"]}`}>
                         <img src="./images/gateway.png" alt="gateway image" />
                     </div>
                 </div>
